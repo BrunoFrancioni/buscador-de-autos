0e4967bbebb42b57c981b177cdd5e10b390aeeb8
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-04-13 22:00:45 -0300

    Proyecto finalizado.

--- js/buscador.js
+++ js/buscador.js
@@ -39,8 +39,54 @@ marca.addEventListener('input', (e) => {
     filtrarAuto();
 });
 
+const year = document.querySelector('#year');
+year.addEventListener('input', (e) => {
+    datosBusqueda.year = Number(e.target.value);
+
+    filtrarAuto();
+});
+
+const minimo = document.querySelector('#minimo');
+minimo.addEventListener('input', (e) => {
+    datosBusqueda.minimo = Number(e.target.value);
+
+    filtrarAuto();
+});
+
+const maximo = document.querySelector('#maximo');
+maximo.addEventListener('input', (e) => {
+    datosBusqueda.maximo = Number(e.target.value);
+
+    filtrarAuto();
+});
+
+const puertas = document.querySelector('#puertas');
+puertas.addEventListener('input', (e) => {
+    datosBusqueda.puertas = Number(e.target.value);
+
+    filtrarAuto();
+});
+
+const transmision = document.querySelector('#transmision');
+transmision.addEventListener('input', (e) => {
+    datosBusqueda.transmision = e.target.value;
+
+    filtrarAuto();
+});
+
+const color = document.querySelector('#color');
+color.addEventListener('input', (e) => {
+    datosBusqueda.color = e.target.value;
+
+    filtrarAuto();
+});
+
+//Funciones
+
 function mostrarAutos(autos) {
     const contenedor = document.getElementById('resultado');
+
+    limpiarHTML();
 
     autos.forEach(auto => {
         const autoHTML = document.createElement('p');
@@ -52,6 +98,85 @@ function mostrarAutos(autos) {
 }
 
 function filtrarAuto() {
-    const resultado = obtenerAutos();
+    const resultado = obtenerAutos().filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
     
+    if(resultado.length) {
+        mostrarAutos(resultado);
+    } else {
+        noResultado();
+    }
+}
+
+function noResultado() {
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.appendChild(document.createTextNode('No hay resultados'));
+
+    document.querySelector('#resultado').appendChild(noResultado);
+}
+
+function limpiarHTML() {
+    const contenedor = document.getElementById('resultado');
+
+    while(contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild);
+    }
+}
+
+function filtrarMarca(auto) {
+    if(datosBusqueda.marca) {
+        return auto.marca === datosBusqueda.marca;
+    } else {
+        return auto;
+    }
+}
+
+function filtrarYear(auto) {
+    if(datosBusqueda.year) {
+        return auto.year === datosBusqueda.year;
+    } else {
+        return auto;
+    }
+}
+
+function filtrarMinimo(auto) {
+    if(datosBusqueda.minimo) {
+        return auto.precio >= datosBusqueda.minimo;
+    } else {
+        return auto;
+    }
+}
+
+function filtrarMaximo(auto) {
+    if(datosBusqueda.minimo) {
+        return auto.precio <= datosBusqueda.minimo;
+    } else {
+        return auto;
+    }
+}
+
+function filtrarPuertas(auto) {
+    if(datosBusqueda.puertas) {
+        return auto.puertas === datosBusqueda.puertas;
+    } else {
+        return auto;
+    }
+}
+
+function filtrarTransmision(auto) {
+    if(datosBusqueda.transmision) {
+        return auto.transmision === datosBusqueda.transmision;
+    } else {
+        return auto;
+    }
+}
+
+function filtrarColor(auto) {
+    if(datosBusqueda.color) {
+        return auto.color === datosBusqueda.color;
+    } else {
+        return auto;
+    }
 }
